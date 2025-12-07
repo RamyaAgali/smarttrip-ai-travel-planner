51a89a6026f1821092de9bea5ca0c0d284cae818
RamyaAgali <ramyashreeagali@gmail.com>
2025-12-07 21:34:07 +0530

    added new comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,45 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import App from './App.jsx'
-// import Signup from './Signup.jsx'
-// import Dashboard from './Dashboard.jsx'
-// import ForgotPassword from './Forgotpassword.jsx'
-// import ResetPassword from './ResetPassword.jsx'
-// import TravelAssistant from './TravelAssistant.jsx'
-// import Profile from './profile.jsx'
-// import MyTrips from './MyTrips.jsx'
-// import BackToDashboardButton from './components/BackToDashboardButton.jsx'
-// import PaymentPage from './PaymentPage.jsx'
-// import AIAssistant from './components/AIAssistant.jsx'
-// import PaymentSuccess from './PaymentSuccess.jsx'
-// import PaymentFailure from './PaymentFailure.jsx'
-// import ProtectedLayout from './components/ProtectedLayout.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//     <div className="relative min-h-screen">
-//       <Routes>
-//         <Route path="/" element={<App />} />
-//         <Route path="/signup" element={<Signup />} />
-//         <Route path="/dashboard" element={<Dashboard/>} />
-//         <Route path='/forgot-password' element={<ForgotPassword/>} />
-//         <Route path='/reset-password' element={<ResetPassword/>} />
-//         <Route path='/travel-assistant' element={<TravelAssistant/>} />
-//         <Route path='/Profile' element={<Profile/>}/>
-//         <Route path='/my-trips' element={<MyTrips/>}/>
-//         <Route path='/Back-to-dashboard' element={<BackToDashboardButton/>}/>
-//         <Route path='/payment/:tripId' element={<PaymentPage/>}/>
-//         <Route path='/payment/success' element={<PaymentSuccess/>}/>
-//         <Route path='/payment/failure' element={<PaymentFailure/>}/>
-//       </Routes>
-//        {/*<AIAssistant/>*/}
-//     </div>
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// )
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -88,3 +46,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
+//force deploy
